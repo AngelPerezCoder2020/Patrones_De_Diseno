bccd42854119bb9cc01fc9a35b1e67cd29457e33
AngelPerezCoder2020 <angelperez.cmp.b.16@gmail.com>
2021-08-19 16:25:47 -0600

    cambios en el main

--- AbstractFactory/src/main/java/MainPackage/Agencia.java
+++ AbstractFactory/src/main/java/MainPackage/Agencia.java
@@ -1,5 +1,26 @@
 package MainPackage;
+import java.util.Scanner;
 
 public class Agencia {
-    public AbstractFactory fabrica;
+    public static AbstractFactory fabrica;
+    public static String opc;
+    public static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args){
+        System.out.println("Bienvenido a la Agencia \n\nQue clase de transimision en Vehiculos Busca?\n 1. Automatica \n 2. Standar");
+        opc = scanner.nextLine();
+        if(opc.equals("1")){
+            fabrica = new FactoryAutomatico();
+            System.out.println("Ingrese el tipo de Vehiculo que busca: \n1. Carro \n 2. Moto");
+        }else if(opc.equals("2")){
+            fabrica = new FactoryStandar();
+            System.out.println("Ingrese el tipo de Vehiculo que busca: \n1. Carro \n 2. Moto");
+        }
+        opc = scanner.nextLine();
+        if(opc.equals("1")){
+            System.out.println("Usted a Recibido: "+fabrica.getCarro().toString());
+        }else if(opc.equals("2")){
+            System.out.println("Usted a Recibido: "+fabrica.getMoto().toString());
+        }
+        scanner.nextLine();
+    }
 }
